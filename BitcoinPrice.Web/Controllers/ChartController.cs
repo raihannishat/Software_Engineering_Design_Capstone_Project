9509c9575e4e731c4f3bb0a58b19528c133ff40e
raihannishat <raihannishat.swe@gmail.com>
2021-04-24 08:31:41 +0600

    Code

--- BitcoinPrice.Web/Controllers/ChartController.cs
+++ BitcoinPrice.Web/Controllers/ChartController.cs
@@ -25,7 +25,32 @@ namespace BitcoinPrice.Web.Controllers
                 .Include( be => be.bpi.EUR)
                 .Include( be => be.bpi.GBP)
                 .Include(y => y.time).OrderByDescending( t => t.time.updatedISO).ToList();
-                                         
+
+            var avgUsd = _dbContext.BitCoinPrice.Include(a => a.bpi.USD).Average(x => x.bpi.USD.rate_float);
+            var avgEur = _dbContext.BitCoinPrice.Include(a => a.bpi.EUR).Average(x => x.bpi.EUR.rate_float);
+            var avgPound = _dbContext.BitCoinPrice.Include(a => a.bpi.GBP).Average(x => x.bpi.GBP.rate_float);
+
+            var minUsd = _dbContext.BitCoinPrice.Include(a => a.bpi.USD).Min(x => x.bpi.USD.rate_float);
+            var minEur = _dbContext.BitCoinPrice.Include(a => a.bpi.EUR).Min(x => x.bpi.EUR.rate_float);
+            var minPound = _dbContext.BitCoinPrice.Include(a => a.bpi.GBP).Min(x => x.bpi.GBP.rate_float);
+
+            var maxUsd = _dbContext.BitCoinPrice.Include(a => a.bpi.USD).Max(x => x.bpi.USD.rate_float);
+            var maxEur = _dbContext.BitCoinPrice.Include(a => a.bpi.EUR).Max(x => x.bpi.EUR.rate_float);
+            var maxPound = _dbContext.BitCoinPrice.Include(a => a.bpi.GBP).Max(x => x.bpi.GBP.rate_float);
+
+            ViewBag.avgEur = avgEur;
+            ViewBag.avgUsd = avgUsd;
+            ViewBag.avgGbp = avgPound;
+
+            ViewBag.maxUsd = maxUsd;
+            ViewBag.maxEur = maxEur;
+            ViewBag.maxGbp = maxPound;
+
+            ViewBag.minUsd = minUsd;
+            ViewBag.minEur = minEur;
+            ViewBag.minGbp = minPound;
+
+
             return View(model);
         }
     }
